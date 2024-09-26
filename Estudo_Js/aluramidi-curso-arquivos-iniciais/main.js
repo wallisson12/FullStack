f927366ad35378d0c63fd95e4eb21207d2665d70
wallisson12 <wallisson_wjc@outlook.com.br>
2024-09-26 15:48:50 -0300

    Projeto basico de js

--- Estudo_Js/aluramidi-curso-arquivos-iniciais/main.js
+++ Estudo_Js/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,25 +1,53 @@
-const ButtonList = document.querySelectorAll('.tecla');
 
 function PlaySound(idSoundElement)
 {
-    document.querySelector(idSoundElement).play();
+    const element = document.querySelector(idSoundElement);
+    
+    if(element != null && element.localName === 'audio')
+    {  
+        element.play();
+    }
+    else
+    {
+        console.log("Teste");
+    }
 }
 
 //Para passar para o evento, usamos sem as '()', para indicar que queremos guardar nela e nao executar
 //Para resolver isso utilizamos funcoes anonimas
 //document.querySelector('.tecla_pom').onclick = PlaySoundPom; 
 
-let i = 0
-while(i<ButtonList.length)
+const ButtonList = document.querySelectorAll('.tecla');
+
+for(let i = 0; i< ButtonList.length; i++)
 {
     const currentButton = ButtonList[i];
 
-    // O classlist pega as classes que tem no seletor
+    // O classlist pega as classes que tem no seletor, como um arry que tem sua posicao
     const idSound = currentButton.classList[1];
     
-    //Funcao anonima
-    currentButton.onclick = function (){PlaySound(`#som_${idSound}`)};
-    i++;
+    //Eventos
+    //Funcao anonima, utilizando quando voce precisa passar o paramentro sem chamar a funcao
+    currentButton.onclick = function (){PlaySound(`#som_${idSound}`);}
+
+    //O parametro 'event' vai possuir o evento que foi acionado
+    //Pegando o keybord
+    currentButton.onkeydown = function(event)
+    {
+        //===, ele alem de comparar o valor, compara tambem o tipo 
+        if(event.code === 'Space' || event.code === 'Enter')
+        {
+            currentButton.classList.add('ativa');
+        }    
+    }
+
+    currentButton.onkeyup = function()
+    {
+        if(!currentButton.classList.contains('ativa'))
+        {
+            currentButton.classList.remove('ativa');
+        }
+    }
 }
 
 
