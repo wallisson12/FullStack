cd4d14058fcb733662972fd9d315a7e232416acb
wallisson12 <wallisson_wjc@outlook.com.br>
2024-10-02 09:13:40 -0300

    Logica do jogo em js pronto

--- Estudo_Js/Js_Com_Jquery/alura-typer-inicial/alura-typer/public/js/main.js
+++ Estudo_Js/Js_Com_Jquery/alura-typer-inicial/alura-typer/public/js/main.js
@@ -1,52 +1,104 @@
 
-let texto = $(".frase").text();
-let tamanhoFrase = $('#tamanho-frase');
-
-let numeroPalavras = texto.split(' ').length;
-
-tamanhoFrase.text(numeroPalavras);
-
+const contadorValorInicial = $("#contador-segundos").text();
 let campo = $('.texto-digitacao');
+let contadorPalavras = $('#contador-palavras');
+let contadorCaracters = $('#contador-caracteres');
+let botaoReiniciar = $('#botao-reiniciar');
 
-campo.on("input",function(){
-
-    let contadorPalavras = $('#contador-palavras');
-    let contadorCaracters = $('#contador-caracteres');
-
-    //Pega o conteudo de um input
-    let conteudo = campo.val();
-
-    let qtdPalavras = conteudo.split(/\S+/).length - 1;
-    let qtdCaracters = conteudo.length; 
-
-
-    contadorPalavras.text(qtdPalavras);
-    contadorCaracters.text(qtdCaracters);
+//chamado quando o documento esta pronto para uso, posso usar so $(function(){logica}), que faz a mesma coisa
+$(document).ready(function()
+{   
+    AtualizaTamnahoFrase();
+    InicializaContador();
+    InicializaConometro();
     
+    //Outra maneira de chamar o evento de click
+    botaoReiniciar.click(ReiniciaGame);
+
 });
 
 
-let contadorSegundos = $("#contador-segundos").text();
+//Atualiza o tamnaho do texto para escrever
+function AtualizaTamnahoFrase()
+{   
+    //Pegando a referencia
+    let texto = $(".frase").text();
+    let tamanhoFraseTxt = $('#tamanho-frase');
 
-//Vai ser chamado uma vez
-campo.one("focus",function(){
+    //Adcionando o tamanho
+    let numeroPalavras = texto.split(' ').length;
+    tamanhoFraseTxt.text(numeroPalavras);
+}
 
-    //Vai ser chamado a cada 1s
-    const id = setInterval(function(){
+function InicializaContador()
+{
+    campo.on("input",function(){
         
-        let current = contadorSegundos--;
-        $("#contador-segundos").text(current);
+        //Pega o conteudo de um input
+        let conteudo = campo.val();
+    
+        let qtdPalavras = conteudo.split(/\S+/).length - 1;
+        let qtdCaracters = conteudo.length; 
+    
+    
+        contadorPalavras.text(qtdPalavras);
+        contadorCaracters.text(qtdCaracters);
+        
+    });
+}
 
-            if(current < 1)
-            {
-                campo.attr("disabled",true);
-                clearInterval(id);
-            }
 
-    },1000)
+function InicializaConometro()
+{
+    let contadorSegundos = $("#contador-segundos").text();
+    
+    //Vai ser chamado uma vez
+    campo.one("focus",function(){
+    
+        //Habilita o botao de reiniciar
+        botaoReiniciar.attr('disabled',true);
 
-});
+        //Vai ser chamado a cada 1s
+        const id = setInterval(function(){
+            
+            let current = contadorSegundos--;
+            $("#contador-segundos").text(current);
+    
+                if(current < 1)
+                {
+                    botaoReiniciar.removeAttr('disabled');
 
+                    //Adciono um elemento na minha tag html e ja pasa o valor
+                    campo.attr("disabled",true);
+    
+                    //Paro a execucao do update
+                    clearInterval(id);
+                }
+    
+        },1000)
+    
+    });
+}
+
+
+function ReiniciaGame()
+{
+    //Removendo a disabilitacao do input
+    campo.attr('disabled',false);
+
+    //Lipando o campo
+    campo.val("");
+
+    //Zerando os contadores
+    contadorCaracters.text("0");
+    contadorPalavras.text("0");
+    
+    //Voltando o tempo inicial
+    $("#contador-segundos").text(contadorValorInicial);
+
+    //Voltando a contabilizar o tempo    
+    InicializaConometro();
+}
 
 
 
