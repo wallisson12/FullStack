757891de29bf169718da47ec6928198593b255a7
wallisson12 <wallisson_wjc@outlook.com.br>
2024-10-08 14:03:20 -0300

    Basico de php

---
+++ Estudo_Php/Basico.php
@@ -0,0 +1,22 @@
+<?php
+
+//O tipo da variavel eh declado quando recebe o valor
+
+//Declaracao de variavel
+$idade = 10;
+$idade = 200;
+
+//Vai ser um double
+$teste = 1.1;
+
+//Printa algo na tela
+//Realizao a concatenacao
+echo 'Teste: ' . PHP_EOL;
+echo "teste: $idade"; 
+
+
+//Retorna o tipo da variavel
+echo gettype($teste);
+
+
+
---
+++ Estudo_Php/TomadaDesicao.php
@@ -0,0 +1,8 @@
+<?php
+
+$idade = 13;
+
+if($idade >= 18)
+{
+    echo "teste";
+}
