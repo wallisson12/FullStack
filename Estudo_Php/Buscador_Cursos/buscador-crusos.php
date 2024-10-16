5f6abf683e7a7f9d8c23fa6547fb19ac54bce16e
wallisson12 <wallisson_wjc@outlook.com.br>
2024-10-16 09:29:51 -0300

    Adcionando o gitIginore

--- Estudo_Php/Buscador_Cursos/buscador-crusos.php
+++ Estudo_Php/Buscador_Cursos/buscador-crusos.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'vendor/autoload.php';
-require_once 'src/Buscador.php';
 
 
 use Alura\BuscadorDeCursos\Buscador;
--- Estudo_Php/Buscador_Cursos/vendor/composer/autoload_static.php
+++ Estudo_Php/Buscador_Cursos/vendor/composer/autoload_static.php
@@ -37,6 +37,10 @@ class ComposerStaticInit7e37f7a74d916cc591f467c71df280bf
             'GuzzleHttp\\Promise\\' => 19,
             'GuzzleHttp\\' => 11,
         ),
+        'A' => 
+        array (
+            'Alura\\BuscadorDeCursos\\' => 23,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -80,6 +84,10 @@ class ComposerStaticInit7e37f7a74d916cc591f467c71df280bf
         'GuzzleHttp\\' => 
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
+        ),
+        'Alura\\BuscadorDeCursos\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
         ),
     );
 
